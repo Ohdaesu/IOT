85cf1f1f3bad95a9ec9b55366314c938740546b7
DESKTOP-2A6QO9P\Franchu <esfrantorre@hotmail.com>
2017-08-13 19:23:02 -0300

    Carpeta Assets

---
+++ Assets/Scripts/CubeActions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CubeActions : MonoBehaviour, IPointerClickHandler {
+
+	private Transform cubeTransform;
+
+	// Use this for initialization
+	void Start () {
+		cubeTransform = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnPointerClick (PointerEventData evd)
+	{
+		Camera.main.GetComponent<CameraControll> ().setLookAt (cubeTransform);
+	}
+}
