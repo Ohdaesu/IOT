9cd9da541d3dedb81f8a1e8e2bca062b8ab6fd72
DESKTOP-2A6QO9P\Franchu <esfrantorre@hotmail.com>
2017-08-14 19:37:42 -0300

    Ultimos cambios

--- Assets/Scripts/rotator.cs
+++ Assets/Scripts/rotator.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class rotator : MonoBehaviour {
-	public Vector3 rotation;
+	public int speed;
+	private Vector3 rotation;
 
+	void Start () {
+		rotation = new Vector3 (0,speed,0);
+	}
 	// Update is called once per frame
 	void Update () {
 		transform.Rotate (rotation * Time.deltaTime);
