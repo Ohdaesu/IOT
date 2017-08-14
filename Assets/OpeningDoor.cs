5c032f485c9bc88b7ab05a167da8660cc9c3c268
DESKTOP-2A6QO9P\Franchu <esfrantorre@hotmail.com>
2017-08-14 13:04:40 -0300

    Las puertas se abren

---
+++ Assets/OpeningDoor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class OpeningDoor : MonoBehaviour, IPointerClickHandler {
+
+	private bool open = false;
+	private Animation _animation;
+
+	// Use this for initialization
+	void Start () {
+		_animation = GetComponent<Animation> ();
+	}
+
+	public void OnPointerClick (PointerEventData evd)
+	{
+		if (!open) { //close door
+			_animation.Play("DoorOpen");
+			open = true;
+		} else { //open door
+			_animation.Play("DoorClose");
+			open = false;
+		}
+	}
+}
