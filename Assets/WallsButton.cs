69668745ac56cc6ab579f8a8606efabbf3a9e9a8
DESKTOP-2A6QO9P\Franchu <esfrantorre@hotmail.com>
2017-08-15 00:08:27 -0300

    Arreglada la casa. La UI se adapta al tamaño de la pantalla

--- Assets/WallsButton.cs
+++ Assets/WallsButton.cs
@@ -1,12 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public class WallsButton : MonoBehaviour  {
 
 	public GameObject casa;
-	private bool show = false;
+	private bool show = true;
+
+	public void Start()
+	{
+		Button btn = GetComponent<Button>();
+		btn.onClick.AddListener (onClickWalls);
+
+	}
 
 	public void onClickWalls ()
 	{
